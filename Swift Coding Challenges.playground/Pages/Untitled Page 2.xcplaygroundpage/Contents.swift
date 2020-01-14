cdd8916a3cd3d9d952fc7ad42558bd472dd919e5
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-14 12:57:43 -0800

    [add] played around with map and reduce functions

--- Swift Coding Challenges.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
-var str = "Hello, playground"
+let numbers = [1,2,3,4]
+let numbersSquared = numbers.map({$0 * $0})
+print("Nums Squared = ",numbersSquared)
+
+let numbersSum = numbers.reduce(0) {
+    $0 + $1
+}
+print("Nums Sum = ", numbersSum)
+
+let numbersProduct = numbers.reduce(1,*)
+print("Nums Product = ", numbersProduct)
+
+
+
+
+
+
 
 //: [Next](@next)

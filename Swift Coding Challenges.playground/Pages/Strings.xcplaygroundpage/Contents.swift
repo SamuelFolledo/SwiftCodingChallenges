37596c5dffd37ffdcfd2ee431fa111cc4d7a2db7
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-16 23:52:10 -0400

    [add] solution to challenge 11

--- Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -173,8 +173,24 @@ func challenge10c(input: String) -> (vowels: Int, consonants: Int) {
 }
 
 
-//: ### Challenge 11:
-//:
+//: ### Challenge 11: Three different letters
+//: Write a function that accepts two strings, and returns true if they are identical in length but have no more than three different letters, taking case and string order into account.
+func challenge11(first: String, second: String) -> Bool {
+    guard first.count == second.count else { return false }
+    let firstArray = Array(first)
+    let secondArray = Array(second)
+    var differences = 0
+    for (index, letter) in firstArray.enumerated() {
+        if secondArray[index] != letter {
+            differences += 1
+            if differences == 4 {
+                return false
+            }
+        }
+    }
+    return true
+}
+
 
 //: ### Challenge 12:
 //:

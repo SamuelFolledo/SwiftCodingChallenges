a9fe1686fe94fe50886a306bd0f691130bd26ecd
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-22 18:20:32 -0400

    [add] challenge 13

--- Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -230,8 +230,30 @@ public extension StringProtocol {
     }
 }
 
-//: ### Challenge 13:
-//:
+//: ### Challenge 13: Run-length encoding
+//: Write a function that accepts a string as input, then returns how often each letter is repeated in a single run, taking case into account.
+func challenge13a(input: String) -> String {
+    var currentLetter: Character?
+    var returnValue = ""
+    var letterCounter = 0
+    for letter in input {
+        if letter == currentLetter {
+            letterCounter += 1
+        } else {
+            if let current = currentLetter {
+                returnValue.append("\(current)\(letterCounter)")
+            }
+            currentLetter = letter
+            letterCounter = 1
+        }
+    }
+    if let current = currentLetter {
+          returnValue.append("\(current)\(letterCounter)")
+    }
+    return returnValue
+}
+   
+
 
 //: ### Challenge 14:
 //:

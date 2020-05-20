a3092acc0e4d873b8568681fca9675e11a6d86fd
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-19 21:36:18 -0400

    [add] setup challenge 15

--- Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -301,6 +301,19 @@ challenge14(string: "123")
 
 //: ### Challenge 15:
 //:
+//Challenge 15: Reverse the words in a string
+//Difficulty: Taxing
+//Write a function that returns a string with each of its words reversed but in the original order, without using a loop.
+//Sample input and output
+//• The string “Swift Coding Challenges” should return “tfiwS gnidoC segnellahC”.
+//• The string “The quick brown fox” should return “ehT kciuq nworb xof”.
+/*Hints
+ Hint #1: You should start by converting the string into an array by separating on spaces.
+ Hint #2: You can reverse the characters in a string by calling reversed() on it.
+ Hint #3: You can create a string from a character array.
+ Hint #4: You want to convert an array of left to right strings into an array of right to left strings, all without using a loop - this is a perfect use for map().
+ Hint #5: Once you have an array of reversed strings, you can create a single string using joined().
+*/
 
 //: ### Challenge 16:
 //:

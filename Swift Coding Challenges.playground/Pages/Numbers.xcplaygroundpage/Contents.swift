5ff22b76b0ae2bcef93ad4da00eea8a0fc7bbe64
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-25 17:18:24 -0400

    [add] setup challenge 22

--- Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -333,6 +333,26 @@ func challenge21b(number: Int) -> (nextHighest: Int?, nextLowest: Int?) {
 
 
 /* ## Challenge 22: Binary reverse
+ 
+ Difficulty: Tricky
+ Create a function that accepts an unsigned 8-bit integer and returns its binary reverse, padded so that it holds precisely eight binary digits.
+ Tip: When you get the binary representation of a number, Swift will always use as few bits as possible – make sure you pad to eight binary digits before reversing.
+ ### Sample input and output
+ - The number 32 is 100000 in binary, and padded to eight binary digits that’s 00100000.
+ Reversing that binary sequence gives 00000100, which is 4. So, when given the input 32 your function should return 4.
+ - The number 41 is 101001 in binary, and padded to eight binary digits that 00101001.
+ Reversing that binary sequence gives 10010100, which is 148. So, when given the input 41 your function should return 148.
+ - It should go without saying that your function should be symmetrical: when fed 4 it
+ should return 32, and when fed 148 it should return 41.
+ 
+ ### Hints
+ Hint #1: You can get the binary representation of an integer using String(someNumber, radix: 2).
+ 
+ Hint #2: You can get the decimal integer equivalent of a string containing binary using Int(someString, radix: 2) – but be warned that will given you an optional integer.
+ 
+ Hint #3: To pad the input number’s binary representation so that it holds eight digits, use the String(repeating:count:) initializer.
+ 
+ Hint #4: You can reverse a character array then create a new string from it.
 
 
 */

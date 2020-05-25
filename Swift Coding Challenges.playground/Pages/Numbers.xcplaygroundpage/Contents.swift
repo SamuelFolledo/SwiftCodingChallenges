719a61c18d7e86e66c610fb26fbb2cb05bd2a743
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-24 22:32:37 -0400

    [add] solution to challenge 21

--- Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -283,6 +283,31 @@ func challenge20b(number: Int) -> Bool {
  Hint #6: You can’t create ranges where the end is higher than the start. Instead, create a forwards range then reverse it.
 */
 
+func challenge21(number: Int) -> (nextHighest: Int?, nextLowest: Int?) {
+    let targetBinary = String(number, radix: 2)
+    let targetOnes = targetBinary.filter { (char: Character) -> Bool in char == "1" }.count
+    var nextHighest: Int? = nil
+    var nextLowest: Int? = nil
+    for i in number + 1...Int.max {
+        let currentBinary = String(i, radix: 2)
+        let currentOnes = currentBinary.filter { (char: Character) -> Bool in char == "1" }.count
+        if targetOnes == currentOnes {
+            nextHighest = i
+            break
+        }
+    }
+    for i in (0 ..< number).reversed() {
+        let currentBinary = String(i, radix: 2)
+        let currentOnes = currentBinary.filter { (char: Character) -> Bool in char == "1" }.count
+        if targetOnes == currentOnes {
+            nextLowest = i
+            break
+        }
+    }
+    return (nextHighest, nextLowest)
+}
+
+
 
 /* ## Challenge 22: Binary reverse
 

f7e09e56c9d539bd959e2d38f4c3b382b2cb2e05
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-04 02:17:14 -0400

    [add] binary search solution

--- Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -498,6 +498,26 @@ func challenge25a(input: Int) -> Int {
     return 0
 }
 
+func challenge19b(input: Int) -> Int {
+    guard input != 1 else { return 1 }
+    var lowerBound = 0
+    var upperBound = 1 + input / 2
+    while lowerBound + 1 < upperBound {
+        let middle = lowerBound + ((upperBound - lowerBound) / 2)
+        let middleSquared = middle * middle
+        if middleSquared == input {
+            return middle
+        } else if middleSquared < input {
+            lowerBound = middle
+            
+        } else {
+            upperBound = middle
+            
+        }
+    }
+    return lowerBound
+}
+
 
 /* ## Challenge 26: Subtract without subtract
  

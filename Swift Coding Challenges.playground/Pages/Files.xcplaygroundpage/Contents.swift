63889384a620a95fb30b89332a1144bff5c2225b
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-13 17:37:50 -0400

    [add] add solution to challenge 27

--- Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
@@ -8,6 +8,18 @@ Write a function that accepts a filename on disk, then prints its last  N lines 
  - Hint #2: Arrays have a built-in ```reverse()``` method that flip them around in-place.
  - Hint #3: You need to print the last  N lines, but of course you don’t want to read beyond the size of the array. Make sure you use the ```min()``` function to choose the lesser of the two.
 */
+import UIKit
+
+func challenge27(filename: String, lineCount: Int) {
+    guard let path = Bundle.main.path(forResource: filename, ofType: nil) else { return }
+    guard let input = try? String(contentsOfFile: path, encoding: .utf8) else { return }
+    var lines = input.components(separatedBy: "\n")
+    guard lines.count > 0 else { return }
+    lines.reverse()
+    for i in 0 ..< min(lines.count, lineCount) {
+        print(lines[i])
+    }
+}
 
 
 //: [Next](@next)

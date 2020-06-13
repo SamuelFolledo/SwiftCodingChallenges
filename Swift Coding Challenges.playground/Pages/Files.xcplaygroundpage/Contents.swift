e5e6770df5a005f5fa9588f51f94be7fd229a444
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-12 22:20:49 -0400

    [add] new page for files algorithms

---
+++ Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+//: [Previous](@previous)
+/*: ## Challenge 27: Print last lines
+Difficulty: Easy
+Write a function that accepts a filename on disk, then prints its last  N lines in reverse order, all on a single line separated by commas.
+*/
+//: [Next](@next)

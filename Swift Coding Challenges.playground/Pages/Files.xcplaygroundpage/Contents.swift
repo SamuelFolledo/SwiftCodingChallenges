4e4adcdb5243eced364ae83fd6599bcc2502c091
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-14 03:02:52 -0400

    [add] setup challenge 28

--- Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
@@ -21,5 +21,28 @@ func challenge27(filename: String, lineCount: Int) {
     }
 }
 
+/*:
+ ## Challenge 28: Log a message
+ Difficulty: Easy
+ 
+ Write a logging function that accepts accepts a path to a log file on disk as well as a new log message. Your function should open the log file (or create it if it does not already exist), then append the new message to the log along with the current time and date.
+ 
+ ### Tip:
+ 
+ It’s important that you add line breaks along with each message, otherwise the log will just become jumbled.
+ 
+ ### Sample input and output
+ 
+ - If the file does not exist, running your function should create it and save the new message
+ 
+ - If it does exist, running your function should load the existing content and append the message to the end, along with suitable line breaking.
+ 
+ ### Hints
+ - Hint #1: I think it would be reasonable to use contentsOfFile to load the existing log file into a string.
+ - Hint #2: You can use try? and nil coalescing to provide a default value if the existing log file doesn’t exist.
+ - Hint #3: How you format the date and time for each message will be interesting, but don’t forget KISS: Keep It Simple, Stupid.
+ - Hint #4: What should happen if you can’t write the log file? This wasn’t specified in the challenge description, so you get to use some initiative.
+ */
+
 
 //: [Next](@next)

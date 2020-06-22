a27ffdaace0030cae55fb65f9e01c2f51f1bcda7
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-21 21:16:00 -0400

    [add] testing challenge

--- Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Files.xcplaygroundpage/Contents.swift
@@ -116,8 +116,10 @@ func challenge30(in directory: String) -> [String] {
     let fm = FileManager.default
     let directoryURL = URL(fileURLWithPath: directory)
     guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return [] }
+    print("Files \(files)")
     var jpegs = [String]()
     for file in files {
+        print("FILE \(file.absoluteString) \(file)")
         if file.pathExtension == "jpg" || file.pathExtension == "jpeg" {          guard let attributes = try? fm.attributesOfItem(atPath: file.path) else { continue }
             guard let creationDate = attributes[.creationDate] as? Date else { continue }
             if creationDate > Date(timeIntervalSinceNow: -60 * 60 * 48) {             jpegs.append(file.lastPathComponent)

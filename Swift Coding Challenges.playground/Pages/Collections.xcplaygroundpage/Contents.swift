8e9cf3d0ddeeeef73f21328ff1291600e9f3700e
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-23 02:13:49 -0400

    [add] question to challenge 44

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -30,24 +30,27 @@ import Foundation
 //[5, 15, 55, 515].challenge37a(count: "5")
 
 
-/*
- Challenge 43: Linked lists
+/*:
+ ## Challenge 43: Linked lists
  Difficulty: Easy
  Create a linked list of lowercase English alphabet letters, along with a method that traverses all nodes and prints their letters on a single line separated by spaces.
- Tip #1: This is several problems in one. First, create a linked list data structure, which itself is really two things. Second, use your linked list to create the alphabet. Third, write a method traverses all nodes and prints their letters.
- Tip #2: You should use a class for this. Yes, really.
- Tip #3: Once you complete your solution, keep a copy of the code – you’ll need it for future challenges!
- Sample input and output
- • The output of your code should be the English alphabet printed to the screen, i.e. “a b c
- d … x y z”.
- Hints Hint #1: If your first instinct was to create your new data types as a struct, it shows you’re a sound Swift developer. Sadly, I’m afraid that approach won’t work here because structs can’t have stored properties that reference themselves.
- Hint #2: Your second instinct might be to use an enum. This makes creation tricksy because you would need to change the associated value after creating the enum.
- Hint #3: Even though this challenge uses alphabet letters, aim to make your class generic – it shows smart forward thinking, and is only fractionally harder than using a specific data type.
- Hint #4: There are lots of hacky ways to loop over the alphabet. The only sensible way is to use "abcdefghijklmnopqrstuvwxyz" – it’s not hard to write, is self-documenting, and quite safe.
- www.hackingwithswift.com
- quite safe.
- Hint #5: You should create two data types: one for a node, which contains its character and link to the next node in the list, and one for the overall linked list, which contains a property for the first node in the list as well as the print method.
- Hint #6: The print() function has a terminator parameter.
+ 
+ #### Tips
+ 
+ 1. This is several problems in one. First, create a linked list data structure, which itself is really two things. Second, use your linked list to create the alphabet. Third, write a method traverses all nodes and prints their letters.
+ 2. You should use a class for this. Yes, really.
+ 3. Once you complete your solution, keep a copy of the code – you’ll need it for future challenges!
+ 
+ #### Sample input and output
+ • The output of your code should be the English alphabet printed to the screen, i.e. “a b c d … x y z”.
+ 
+ #### Hints
+ 1. If your first instinct was to create your new data types as a struct, it shows you’re a sound Swift developer. Sadly, I’m afraid that approach won’t work here because structs can’t have stored properties that reference themselves.
+ 2. Your second instinct might be to use an enum. This makes creation tricksy because you would need to change the associated value after creating the enum.
+ 3. Even though this challenge uses alphabet letters, aim to make your class generic – it shows smart forward thinking, and is only fractionally harder than using a specific data type.
+ 4. There are lots of hacky ways to loop over the alphabet. The only sensible way is to use "abcdefghijklmnopqrstuvwxyz" – it’s not hard to write, is self-documenting, and quite safe.
+ 5. You should create two data types: one for a node, which contains its character and link to the next node in the list, and one for the overall linked list, which contains a property for the first node in the list as well as the print method.
+ 6. The `print()` function has a terminator parameter.
  */
 
 class Node<T> {
@@ -83,3 +86,21 @@ for letter in "abcdefghijklmnopqrstuvwxyz" {
     previousNode = node
 }
 list.printNodes()
+
+/*:
+ ## Challenge 44: Linked list mid-point
+Extend your linked list class with a new method that returns the node at the mid point of the linked list using no more than one loop.
+
+ Tip: If the linked list contains an even number of items, returning the one before or the one after the center is acceptable.
+
+ #### Sample input and output
+- If the linked list contains 1, 2, 3, 4, 5, your method should return 3.
+- If the linked list contains 1, 2, 3, 4, your method may return 2 or 3.
+- If the linked list contains the English alphabet, your method may return M or N.
+
+ #### Hints
+1. It’s easy to solve this in two passes, but only fractionally harder to solve it in one.
+2. If you use fast enumeration – for i in items – you move over one item at a time. Can you think of a way of moving over more than one item?
+3. Once you pull out two items at the same time, you can make them move at different speeds through the list.
+4. If you move pointer A through the list one item at a time, and pointer B through the list two items at a time, by the time pointer B reaches the end where will pointer A be?
+*/

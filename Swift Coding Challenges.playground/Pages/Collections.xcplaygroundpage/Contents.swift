3639649467d2f7cd5d8368bf358e0086933ea527
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-24 23:38:22 -0400

    [add] solution for reversed linked list

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -81,6 +81,31 @@ class LinkedList<T> {
             currentNode = node.next
         }
     }
+    
+    func reversed() -> LinkedList<T> {
+        // create our copy for the return value
+        let copy = LinkedList<T>()
+        // if we have nodes to copy…
+        if let startNode = head {
+            // copy the original start node
+            var previousCopyNode = Node(value: startNode.value)
+            // now start copying from the next node
+            var currentNode = head?.next
+            while let node = currentNode {
+                // create a copy of this node
+                let copyNode = Node(value: node.value)
+                // make it point to the node we created previously
+                copyNode.next = previousCopyNode
+                // then make it the previous node, so we can move forward
+                previousCopyNode = copyNode
+                currentNode = currentNode?.next
+                
+            }
+            // we're at the end of the list, which is our new start
+            copy.head = previousCopyNode
+        }
+        return copy
+    }
 }
 
 var list = LinkedList<Character>()

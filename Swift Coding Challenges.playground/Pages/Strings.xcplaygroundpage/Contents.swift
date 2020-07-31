e67e731517014d4f884d62b1bf1a4afff5ff1f79
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-31 03:53:42 -0400

    [add] best way to validate phone numbers

--- Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -338,3 +338,24 @@ func challenge15(input: String) -> String {
 
 //: ### Challenge 19:
 //:
+
+
+// Phone Regex
+
+extension String {
+    public var validPhoneNumber: Bool {
+        let types: NSTextCheckingResult.CheckingType = [.phoneNumber]
+        guard let detector = try? NSDataDetector(types: types.rawValue) else { return false }
+        if let match = detector.matches(in: self, options: [], range: NSMakeRange(0, self.count)).first?.phoneNumber {
+            return match == self
+        } else {
+            return false
+        }
+    }
+}
+
+print("\("+96 (123) 456-0990".validPhoneNumber)") //returns false, smart enough to know if country phone code is valid as well 🔥
+print("\("+994 (123) 456-0990".validPhoneNumber)") //returns true because +994 country code is an actual country phone code
+print("\("(123) 456-0990".validPhoneNumber)") //returns true
+print("\("123-456-0990".validPhoneNumber)") //returns true
+print("\("1234560990".validPhoneNumber)") //returns true

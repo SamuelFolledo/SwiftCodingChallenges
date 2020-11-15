5898cf77c540bc052871bfcd8b84ea7887fffaf8
Samuel Folledo <samuelfolledo@gmail.com>
2020-11-15 01:56:53 -0500

    [add] questions for challenge 41

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -149,6 +149,29 @@ func challenge40c(input: [Int]) -> [Int] {
     return Array(testSet.subtracting(inputSet)).sorted()
 }
 
+/*
+ ## Challenge 41: Find the median
+ Difficulty: Easy
+ 
+ Write an extension to collections that accepts an array of Int and returns the median average, or nil if there are no values.
+ 
+ **Tip**: The mean average is the sum of some numbers divided by how many there are. The *median* average is the middle of a sorted list. If there is no single middle value – e.g. if there are eight numbers - then the median is the mean of the two middle values.
+ ### Sample input and output
+ - The code `[1, 2, 3].challenge41()` should return 2.
+ - The code `[1, 2, 9].challenge41()` should return 2.
+ - The code `[1, 3, 5, 7, 9].challenge41()` should return 5.
+ - The code `[1, 2, 3, 4].challenge41()` should return 2.5.
+ - The code `[Int]().challenge41()` should return nil.
+ 
+ ### Hints
+ - Hint #1: You’ll need to extend `Collection` with a specific constraint.
+ - Hint #2: The method should return `Double`? because it might be a whole number, it might be a mean average of two numbers, or it might be nil if the collection is empty.
+ - Hint #3: If you divide an odd integer by two, Swift will round down.
+ - Hint #4: If you divide an even-numbered collection’s count by two, you’ll get the highest of the two values you need for your mean.
+ - Hint #5: Make life easy for yourself: sort the collection first.
+
+ */
+
 
 /*:
  ## Challenge 43: Linked lists

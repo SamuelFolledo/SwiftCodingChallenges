7cd0ec2791881cae84a216a1b81eee347c1b9e3c
Samuel Folledo <samuelfolledo@gmail.com>
2020-11-15 01:58:48 -0500

    [add] answer for challenge 41 with comments

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -169,9 +169,23 @@ func challenge40c(input: [Int]) -> [Int] {
  - Hint #3: If you divide an odd integer by two, Swift will round down.
  - Hint #4: If you divide an even-numbered collection’s count by two, you’ll get the highest of the two values you need for your mean.
  - Hint #5: Make life easy for yourself: sort the collection first.
-
  */
 
+extension Collection where Iterator.Element == Int {
+    func challenge41() -> Double? {
+        guard count != 0 else { return nil }
+        // sort the items so we can find the center point
+        let sorted = self.sorted()
+        let middle = sorted.count / 2
+        if sorted.count % 2 == 0 {
+            // return mean average of two center items
+            return Double(sorted[middle] + sorted[middle - 1]) / 2
+        } else {
+            // return the single center item
+            return Double(sorted[middle])
+        }
+    }
+}
 
 /*:
  ## Challenge 43: Linked lists

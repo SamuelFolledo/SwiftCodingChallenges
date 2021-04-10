b1ec3647b5af5f4b643017aec103c6acd2938dae
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-09 23:45:01 -0400

    [add] prompt for challenge 49 and hints

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -723,3 +723,22 @@ struct deque<T> {
         }
     }
 }
+
+
+/*:
+ ## Challenge 49: Sum the even repeats
+ Difficulty: Tricky
+ 
+ Write a function that accepts a variadic array of integers and return the sum of all numbers that appear an even number of times.
+ 
+ ### Sample input and output
+ - The code `challenge49(1, 2, 2, 3, 3, 4)` should return 5, because the numbers 2 and 3 appear twice each.
+ - The code `challenge49(5, 5, 5, 12, 12)` should return 12, because that’s the only number that appears an even number of times.
+ - The code `challenge49(1, 1, 2, 2, 3, 3, 4, 4)` should return 10.
+ ### Hints
+ - Hint #1: This is a perfect use for NSCountedSet.
+ - Hint #2: But: NSCountedSet doesn’t use generics, so you’ll need to typecast somehow. Expect to be judged on your method of typecasting!
+ - Hint #3: You’ll need to use modulus to find numbers that are repeated an even number of times.
+ */
+
+

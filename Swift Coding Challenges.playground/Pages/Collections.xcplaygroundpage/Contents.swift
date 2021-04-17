d98a345f9494a3ac3277c519da6db8f9c558c9ae
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-17 00:39:57 -0400

    [add] questions for challenge 50

--- Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -763,3 +763,25 @@ func challenge49b(numbers: Int...) -> Int {
     }
     return sum
 }
+
+
+/*
+ ## Challenge 50: Count the largest range
+ Difficulty: Tricky
+ 
+ Write a function that accepts an array of positive and negative numbers and returns a closed range containing the position of the contiguous positive numbers that sum to the highest value, or nil if nothing were found.
+ ### Sample input and output
+ - The array `[0, 1, 1, -1, 2, 3, 1]` should return `4...6` because the highest
+ sum of contiguous positive numbers is 2 + 3 + 1, which lie at positions 4, 5, and 6 in the array.
+ - The array `[10, 20, 30, -10, -20, 10, 20]` should return `0...2`.
+ - The array `[1, -1, 2, -1]` should return `2...2`.
+ - The array `[2, 0, 2, 0, 2]` should return `0...0`.
+ - The array `[Int]()` should return `nil`.
+ 
+ ### Hints
+ - Hint #1: This challenge is best solved using a little trial and error – start by writing tests to ensure your solution is good as you work.
+ - Hint #2: Your return type should be `CountableClosedRange<Int>?` because there might not be any ranges of positive numbers.
+ - Hint #3: This would be a good time to use `enumerated()` to retrieve items and their index from a collection.
+ - Hint #4: There are two very different cases: if a number is greater than 0, and “everything else”. The index you return needs to be different, because a positive integer you find might be the last one in the array.
+
+ */

3c4c5255f4421bde09ef2bae2389148eb43e0f2f
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-26 23:29:43 -0400

    [add] fixed string bugs

--- Swift Coding Challenges.playground/Pages/Section 1 - Strings.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 1 - Strings.xcplaygroundpage/Contents.swift
@@ -367,9 +367,9 @@ extension String {
     var isValidPassword: Bool {
         do {
             let regex = try NSRegularExpression(pattern: "^[a-zA-Z_0-9\\-_,;.:#+*?=!§$%&/()@]+$", options: .caseInsensitive)
-            if(regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil){
+            if(regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil){
 
-                if(self.characters.count>=6 && self.count<=20){
+                if(self.count>=6 && self.count<=20){
                     return true
                 }else{
                     return false

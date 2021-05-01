7e3edd4787105f719488544eaa4d555ea5eacfb1
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-01 00:05:41 -0400

    [add] edited prompt for challenge 56

--- Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
@@ -76,17 +76,25 @@ extension Array where Element: Comparable {
 
 [12, 5, 4, 9, 3, 2, 1].challenge55a()
 
-/*
- Challenge 56: Insertion sort
+/*:
+ ## Challenge 56: Insertion sort
  Difficulty: Easy
+ 
  Create an extension for arrays that sorts them using the insertion sort algorithm.
- Tip: An insertion sort creates a new, sorted array by removing items individually from the input array and placing them into the correct position in the new array.
- Sample input and output
- • The array [12, 5, 4, 9, 3, 2, 1] should become [1, 2, 3, 4, 5, 9,
- 12]. • The array ["f", "a", "b"] should become ["a", "b", "f"]. • The array [String]() should become [].
- Hints Hint #1: You can perform insertion sort in-place, but that takes a little more thinking. Aim for correctness first, and efficiency later. Hint #2: You will need to extend Array with a constraint on their elements so that they must be Comparable – that’s what lets us sort items. Hint #3: In the most simple solution, you should be able to pick out an item from your source array, then search through your sorted destination array to find where it should go.
- Hint #4: If you want to try the in-place solution, pull out the current item you want to sort, then keep moving other elements to the right until you find the correct spot for your item.
-
+ 
+ **Tip**: An insertion sort creates a new, sorted array by removing items individually from the input array and placing them into the correct position in the new array.
+ ### Sample input and output
+ - The array `[12, 5, 4, 9, 3, 2, 1]` should become `[1, 2, 3, 4, 5, 9,
+ 12]`.
+ - The array `["f", "a", "b"]` should become `["a", "b", "f"]`.
+ - The array `[String]()` should become `[]`.
+ ### Hints
+ - Hint #1: You can perform insertion sort in-place, but that takes a little more thinking. Aim for correctness first, and efficiency later.
+ - Hint #2: You will need to extend Array with a constraint on their elements so that they must be Comparable – that’s what lets us sort items.
+ - Hint #3: In the most simple solution, you should be able to pick out an item from your source array, then search through your sorted destination array to find where it should go.
+ - Hint #4: If you want to try the in-place solution, pull out the current item you want to sort, then keep moving other elements to the right until you find the correct spot for your item.
  */
+
+
 
 //: [Next](@next)

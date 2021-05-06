948de6fc553a2bee27beca5d0a95316186510e82
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-05 23:28:55 -0400

    [add] solution to challenge 57

--- Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
@@ -140,7 +140,7 @@ extension Array where Element: Comparable {
     }
 }
 
-/*
+/*:
  ## Challenge 57: Isomorphic values
  Difficulty: Easy
  
@@ -175,5 +175,29 @@ extension Array where Element: Comparable {
  - Hint #7: If the character isn’t already a key, and the second string’s letter isn’t already a value, then add the character and matching second string letter to your character mapping dictionary.
  - Hint #8: If you’ve made it through all the characters in the first string and not encountered any problems, you have an isomorphic string.
  */
+
+func challenge57(first firstValue: Any, second secondValue: Any) -> Bool {
+    let first = String(describing: firstValue)
+    let second = String(describing: secondValue)
+    guard first.count == second.count else { return false }
+    var characterMap = [Character: Character]()
+    let firstArray = Array(first)
+    let secondArray = Array(second)
+    for (index, character) in firstArray.enumerated() {
+        let otherCharacter = secondArray[index]
+        if let currentMapping = characterMap[character] {
+            if currentMapping != otherCharacter {
+                return false
+            }
+        } else {
+            if characterMap.values.contains(otherCharacter) {
+                return false
+            }
+            characterMap[character] = otherCharacter
+        }
+    }
+    return true
+}
+
 
 //: [Next](@next)

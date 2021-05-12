ef96f4adddb44a06f2fb69a97a5e89f20c766809
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-12 02:21:45 -0400

    [add] prompt for challenge 59 quicksort

--- Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
@@ -261,6 +261,25 @@ func challenge58(input: String) -> Bool {
 
 print("Challenge 58: \(challenge58(input: "{}(()})"))")
 
+/*:
+ ## Challenge 59: Quicksort
+ Difficulty: Tricky
+ 
+ Create an extension for arrays that sorts them using the quicksort algorithm.
+ - Tip #1: The quicksort algorithm picks an item from its array to use as the pivot point, then splits itself into either two parts (less than or greater than) or three (less, greater, or equal). These parts then repeat the pivot and split until the entire array has been split, then it gets rejoined so that less, equal, and greater are in order.
+ - Tip #2: I can write quicksort from memory, but I cannot write fully optimized quicksort from memory. It’s a complex beast to wrangle, and it requires careful thinking – honestly, I have better things to keep stored in what little space I have up there! So, don’t feel bad if your attempt is far from ideal: there’s no point creating a perfect solution if you struggle to remember it during an interview.
+ - Tip #3: Quicksort is an algorithm so well known and widely used that you don’t even write a space in its name – it’s “quicksort” rather than “quick sort”.
+ ### Sample input and output
+ - The array [12, 5, 4, 9, 3, 2, 1] should become [1, 2, 3, 4, 5, 9, 12].
+ - The array ["f", "a", "b"] should become ["a", "b", "f"].
+ - The array [String]() should become [].
+ 
+ ### Hints
+ - Hint #1: You will need to extend Array with a constraint on their elements so that they must be Comparable – that’s what lets us sort items.
+ - Hint #2: There are lots of ways to pick a pivot point; choosing a random item is probably best.
+ - Hint #3: You can do most of the work with filter() if you want.
+ - Hint #4: For maximum performance, try to solve the challenge in-place using inout.
 
+ */
 
 //: [Next](@next)

758362f3b54ea7054411791cd6727d8671f241a0
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-12 02:58:25 -0400

    [add] iterative solutions for challenge 60

--- Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
@@ -374,5 +374,88 @@ Once you’ve chosen a good pivot point, from the extensive research that’s be
     }
 }
 
+/*
+ ## Challenge 60: Tic-Tac-Toe winner
+ Difficulty: Tricky
+ 
+ Create a function that detects whether either player has won in a game of Tic-Tac-Toe.
+ 
+ **Tip**: A tic-tac-toe board is 3x3, containing single letters that are either X, O, or empty. A win is three Xs or Os in a straight line.
+ ### Sample input and output
+ - The array `[["X", "", "O"], ["", "X", "O"], ["", "", "X"]]` should return true.
+ • The array `[["X", "", "O"], ["X", "", "O"], ["X", "", ""]]` should return true.
+ • The array `[["", "X", ""], ["O", "X", ""], ["O", "X", ""]]` should return true.
+ • The array `[["", "X", ""], ["O", "X", ""], ["O", "", "X"]]` should return false.
+ • The array `[["", "", ""], ["", "", ""], ["", "", ""]]` should return false.
+ ### Hints
+ - Hint #1: Your board parameter should be [[String]] – an array of array of strings.
+ = Hint #2: You can evaluate the rows and columns in a loop.
+ - Hint #3: You can evaluate diagonals using two checks: one from top left to bottom right, and one from bottom left to top right.
+ - Hint #4: You might want to use a nested function to make your code cleaner.
+ */
+
+func challenge60a(_ board: [[String]]) -> Bool {
+    for i in 0 ..< 3 {
+        // check each row
+        if board[i][0] != "" && board[i][0] == board[i][1] && board[i][0] == board[i][2] {
+            return true
+        }
+        // now check diagonally top left to bottom right
+        if board[0][0] != "" && board[0][0] == board[1][1] && board[0][0] == board[2][2] {
+            return true
+        }
+        // and check diagonally bottom left to top right
+        if board[0][2] != "" && board[0][2] == board[1][1] && board[0][2] == board[2][0] {
+            return true
+        }
+        // if we're still here there's no winner
+        // check each column
+        if board[0][i] != "" && board[0][i] == board[1][i] && board[0][i] == board[2][i] {
+            return true
+        }
+    }
+    return false
+}
+
+func challenge60b(_ board: [[String]]) -> Bool {
+    //clean this up, and in doing so make the code much more readable, we can create a nested isWin() function that returns true if all three squares contain a move from the same player
+    func isWin(_ first: String, _ second: String, _ third: String) -> Bool {
+        guard first != "" else { return false }
+        return first == second && first == third
+    }
+    for i in 0 ..< 3 {
+        if isWin(board[i][0], board[i][1], board[i][2]) {
+            return true
+        }
+        if isWin(board[0][i], board[1][i], board[2][i]) {
+            return true
+        }
+    }
+    if isWin(board[0][0], board[1][1], board[2][2]) {
+        return true
+    }
+    if isWin(board[0][2], board[1][1], board[2][0]) {
+        return true
+    }
+    return false
+}
+
+func challenge60c(_ board: [[String]]) -> Bool {
+    func isWin(_ first: String, _ second: String, _ third: String) -> Bool {
+        guard first != "" else { return false }
+        return first == second && first == third
+    }
+    for i in 0 ..< 3 {
+        if isWin(board[i][0], board[i][1], board[i][2]) {
+            return true
+        }
+        if isWin(board[0][i], board[1][i], board[2][i]) {
+            return true
+        }
+    }
+    // if either the first one or the second check is a winning move then they will return true, otherwise false
+    return isWin(board[0][0], board[1][1], board[2][2]) || isWin(board[0][2], board[1][1], board[2][0])
+}
+
 
 //: [Next](@next)

90da9eb0c341a89902da7e3cf0d26ece1ec2c89e
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-12 03:55:31 -0400

    [add] prompt for challenge 62

--- Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Section 5 - Algorithms.xcplaygroundpage/Contents.swift
@@ -506,8 +506,34 @@ func challenge61(upTo max: Int) -> [Int] {
 
 print("Challenge 61: \(challenge61(upTo: 100))")
 
-/*
+/*:
+ ## Challenge 62: Points to angles
+ Difficulty: Tricky
  
+ Write a function that accepts an array of CGPoint pairs, and returns an array of the angles between each point pair. Return the angles in degrees, where 0 or 360 is straight up.
+ 
+ Tip: If it helps your thought process, imagine each point pair as being two touches on the screen: you have the first touch and the second, what is the angle between them?
+ 
+ ### Sample input and output
+ Here is some code you can test with:
+ ```
+ var points = [(first: CGPoint, second: CGPoint)]()
+ points.append((first: CGPoint.zero, second: CGPoint(x: 0, y: -100)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: 100, y: -100)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: 100, y: 0)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: 100, y: 100)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: 0, y: 100)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: -100, y: 100)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: -100, y: 0)))
+ points.append((first: CGPoint.zero, second: CGPoint(x: -100, y: -100)))
+ print(challenge62(points: points))
+ ```
+ If your code has worked correctly, that should print [0.0, 45.0, 90.0, 135.0, 180.0, 225.0, 270.0, 315.0]. Returning 360.0 for the first number is also acceptable.
+ ### Hints
+ - Hint #1: You’re mapping an array of one type to an array of another type. Yes, that means map() is a good idea.
+ - Hint #2: You’ll need to use `atan2()` to calculate the angle between any two points, providing the Y delta and the X delta as its two parameters.
+ - Hint #3: `atan2()` works in radians, so you’ll need to convert the value to degrees using the formula `degrees = radians * 180 / Double.pi`.
+ - Hint #4: Where “0” lies is of course arbitrary, but the convention is that 0 degrees runs along the positive X axis. You need to correct for this.
  */
 
 //: [Next](@next)

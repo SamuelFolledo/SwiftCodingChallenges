ad5cadea1388bde0b0a982fd14329184c0ab7429
SamuelFolledo <samuelfolledo@gmail.com>
2021-05-25 00:10:22 -0400

    [add] solution for two strings

--- Swift Coding Challenges.playground/Pages/HackerRank.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/HackerRank.xcplaygroundpage/Contents.swift
@@ -21,7 +21,7 @@ func hourglassSum(arr: [[Int]]) -> Int {
         for column in 0..<arr[row].count - 2 {
             let hourglass = getHourglass(row: row, column: column)
             let total = hourglass.reduce(0){$0+$1}
-            print("Hourglass \(row), \(column): \(hourglass) = \(total)")
+//            print("Hourglass \(row), \(column): \(hourglass) = \(total)")
             if total > maxSum {
                 maxSum = total
             }
@@ -36,7 +36,7 @@ let arr = [[-9, -9, -9, 1, 1, 1],
            [0, 0, 8, 6, 6, 0],
            [0, 0, 0, -2, 0, 0],
            [0, 0, 1, 2, 4, 0,]]
-print(hourglassSum(arr: arr))
+print("Hourglass Max Sum = ", hourglassSum(arr: arr))
 
 /*:
  ## Two Strings
@@ -57,6 +57,51 @@ print(hourglassSum(arr: arr))
  ```
 
  These do not share a substring.
+ 
+ ### Output Format
+
+ For each pair of strings, return YES or NO.
+
+ ### Sample Input
+ ```
+ 2
+ hello
+ world
+ hi
+ world
+ ```
+ ### Sample Output
+ YES
+ 
+ NO
+ ### Explanation
+
+ We have p = 2 pairs to check:
+ 1. s1 = "hello", s2 = world". The substrings  and  are common to both strings.
+ 2. a= "hi", b = "world". s1 and s2  share no common substrings.
  */
+
+func twoStrings(s1: String, s2: String) -> String {
+    var s1Dic = [String: Int]()
+    for letter in s1 {
+        s1Dic[String(letter)] = 0
+    }
+    for letter in s2 {
+        if let _ = s1Dic[String(letter)] {
+//            s1Dic[String(letter)]! += 1
+            return "YES"
+        }
+    }
+//    for value in s1Dic.values {
+//        if value > 0 {
+//            return "YES"
+//        }
+//    }
+    return "NO"
+}
+
+let s1 = "hello"
+let s2 = "world"
+print("Two Strings for s1 \(s1) and s2 \(s2) = \(twoStrings(s1: s1, s2: s2))")
 
 //: [Next](@next)
